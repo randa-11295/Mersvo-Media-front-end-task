cc4cb6f7381b0c56b16531b3b2d1f70cd8ca5657
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-02-25 13:24:47 +0200

    add theme

---
+++ js/theme.js
@@ -0,0 +1,21 @@
+       //  tailwind theme over write
+       tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    primary: "#e37808",
+                    secondary: "#ffecec",
+                    success: "#34817b",
+               
+                },
+                height: {
+                    "40vh": "40vh",
+                    "70vh": "70vh",
+                },
+                fontSize: {
+                    "2rem": "2rem",
+                },
+                backgroundSize: { fill: "100% 100%" },
+            },
+        },
+    };
